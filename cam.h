ccc99f29cdac6b5c48866db8ba8e7c2d146d09e8
panda5mt <lynxeyed@live.jp>
2022-05-22 04:01:21 +0000

    Automatically uploaded

--- cam.h
+++ cam.h
@@ -21,10 +21,12 @@
 #define CAM_BUF_SIZE    (640 * BLOCK * 2)   // in bytes
 #define CAM_BUF_HALF    (CAM_BUF_SIZE / 2)  // in bytes
 
-
+// low layer APIs
 void iot_sram_init(PIO pio, uint32_t sm);
 void iot_sram_write(PIO pio, uint32_t sm, uint32_t *send_data, uint32_t address, uint32_t length_in_byte, uint32_t dma_channel);
 void *iot_sram_read(PIO pio, uint32_t sm, uint32_t *read_data, uint32_t address, uint32_t length_in_byte, uint32_t dma_channel);
+
+// high layer APIs
 void init_cam(uint8_t DEVICE_IS);
 void config_cam_buffer();
 void start_cam();
