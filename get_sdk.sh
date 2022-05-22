83ec0641672e45c0bef2157cd727a8bf74218e3f
panda5mt <lynxeyed@live.jp>
2022-05-22 05:02:56 +0000

    Automatically uploaded

---
+++ get_sdk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential 
+
+cd ~/
+mkdir pico
+cd pico
+git clone -b master https://github.com/raspberrypi/pico-sdk.git
+cd pico-sdk
+git submodule update --init
+cd ..
+git clone -b master https://github.com/raspberrypi/pico-examples.git
+
+git clone https://github.com/panda5mt/picocam.git
+cd picocam
+echo -e '#!/bin/bash\nNOW=`date "+%Y%m%d_%H%M%S"`\ngit add .\n# git commit -m "automatically uploaded at "$NOW\ngit commit -m "Automatically uploaded"\ngit push origin HEAD' > add_git.sh
+chmod +x add_git.sh
+cp ../pico-sdk/external/pico_sdk_import.cmake .
+echo -e '#!/bin/bash\nmkdir -p build\ncd build\nexport PICO_SDK_PATH=../../pico-sdk\ncmake ..\nmake' > pico_build.sh 
+chmod +x pico_build.sh
+git config --global core.autocrlf false
