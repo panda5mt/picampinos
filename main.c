5d6a274b46b8a442ed743a428297700d6cf5debc
panda5mt <lynxeyed@live.jp>
2022-08-28 07:28:18 +0000

    Automatically uploaded

--- main.c
+++ main.c
@@ -118,10 +118,10 @@ int main() {
 
     
     // data via USB-UART(ASCII)
-    // see also 'matlab/readrgb.m'    
-    // while(true) {
-    //     uartout_cam();          
-    // }
+    see also 'matlab/readrgb.m'    
+    while(true) {
+        uartout_cam();          
+    }
 
     // data via USB-UART(binary) 
     // see also 'matlab/comm_uart_bin.m'
@@ -147,9 +147,9 @@ int main() {
     // To Use SFP, CHECK 'USE_EZSPI_SLAVE' is (false) 
     // AND 'USE_100BASE_FX' is (true) in 'cam.h'
     
-    while(true) {
-        sfp_cam();
-    }
+    // while(true) {
+    //     sfp_cam();
+    // }
     
     // end
     free_cam();
