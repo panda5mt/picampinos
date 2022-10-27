7ea9529f908d66ca48d02cc7c185091c488f69bb
panda5mt <lynxeyed@live.jp>
2022-10-27 06:40:09 +0000

    Automatically uploaded

--- pico_build.sh
+++ pico_build.sh
@@ -2,3 +2,4 @@
 echo -e "[35m---------------- building firmware in firmware/ ---------------- [m"
 cd firmware
 ./pico_build.sh
+echo -e "[35m---------------- the build bin and uf2 files in firmware/build ---------------- [m"
