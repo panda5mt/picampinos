636a0d64aa5efac2ffad3dba8f6209ad9d615bbd
panda5mt <lynxeyed@live.jp>
2022-10-27 06:57:26 +0000

    Automatically uploaded

--- pico_build.sh
+++ pico_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo -e "[35m---------------- building firmware in firmware/ ---------------- [m"
 cd firmware
-./pico_build.sh
-echo -e "[35m---------------- the build bin and uf2 files in firmware/build ---------------- [m"
+if ./pico_build.sh; then
+echo -e "[35m---------------- build bin and uf2 files in firmware/build ---------------- [m"
+else
+echo -e "\e[31mError. None firmware is built.\e[m"
+fi
