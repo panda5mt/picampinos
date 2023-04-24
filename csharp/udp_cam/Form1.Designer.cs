6ecd46f7f00ece1966e4f62a2a392a23f02fe7ca
Atsushi Saitoh <lynxeyed@live.jp>
2023-04-24 17:06:56 +0900

    added cs code

---
+++ csharp/udp_cam/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace udp_cam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(63, 28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1280, 960);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(63, 1047);
+            button1.Name = "button1";
+            button1.Size = new Size(331, 46);
+            button1.TabIndex = 1;
+            button1.Text = "Connect Camera";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1474, 1129);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ csharp/udp_cam/Form1.cs
@@ -0,0 +1,129 @@
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace udp_cam
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpClient;
+        private Thread receiveThread;
+        private Bitmap image;
+        private object imageLock = new object();
+        private bool formClosed = false;
+
+        private UInt32 frame_start_packet = 0xdeadbeef;
+        private UInt32 header_pixel_data = 0xbeefbeef;
+        private UInt32 frame_end_packet = 0xdeaddead;
+        private UInt32 height, width, sz, fcounter = 0; // height, column, data-size
+        private Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            formClosed = true;
+            udpClient.Close();
+        }
+
+        private void ReceiveData()
+        {
+            bmp = new Bitmap(640, 480); // バイト配列をビットマップに変換
+            while (!formClosed)
+            {
+                try
+                {
+                    IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] data = udpClient.Receive(ref endPoint);
+
+                    // 先頭の0xDEADBEEFをチェック
+                    if (BitConverter.ToUInt32(data, 0) == frame_start_packet)
+                    {
+                        //bmp = new Bitmap(640, 480); // バイト配列をビットマップに変換
+                    }
+                    else if (BitConverter.ToUInt32(data, 0) == header_pixel_data)
+                    {
+                        //Debug.WriteLine("data!");
+                        height = BitConverter.ToUInt32(data, 4) - 1;     //height number
+                        width = BitConverter.ToUInt32(data, 8) - 1;     //columb number
+                        sz = BitConverter.ToUInt32(data, 12) * 2;   // data size
+                        //Debug.WriteLine("height={0}, width={1}, sz={2}", height, width, sz);
+
+                        int index = 16; // 先頭の16バイト分はヘッダなので、indexを16から始める
+
+                        for (int x = (int)width; x < sz; x++)
+                        {
+                            // RGB565のバイナリデータを解析して、カラー情報を取得
+                            uint color = (uint)(data[index] << 8 | data[index + 1]);
+                            uint r = (color >> 11) & 0x1f;
+                            uint g = (color >> 5) & 0x3f;
+                            uint b = color & 0x1f;
+                            r = (r * 255 / 31);
+                            g = (g * 255 / 63);
+                            b = (b * 255 / 31);
+
+                            // ピクセルにカラーをセット
+                            bmp.SetPixel((int)x, (int)height, Color.FromArgb((int)r, (int)g, (int)b));
+
+                            index += 2;
+                        }
+                        fcounter++;
+                    }
+                    //else if (BitConverter.ToUInt32(data, 0) == frame_end_packet)
+                    if (fcounter > 480)
+                    {
+                        fcounter = 0;
+                        lock (imageLock)
+                        {
+                            //if (image == null)
+                            {
+                                image = new Bitmap(bmp);
+                                //bmp.Dispose();
+                            }
+
+
+
+                            pictureBox1.Invoke(new Action(() =>
+                            {
+                                // 2倍に拡大する倍率を定義します。
+                                const int ZoomFactor = 2;
+
+                                // 2倍のサイズで新しいビットマップを作成します。
+                                Bitmap zoomedBitmap = new Bitmap(image.Width * ZoomFactor, image.Height * ZoomFactor);
+
+                                // 新しいビットマップのGraphicsオブジェクトを取得します。
+                                Graphics graphics = Graphics.FromImage(zoomedBitmap);
+
+                                // InterpolationModeをHighQualityBicubicに設定し、画像の拡大品質を向上します。
+                                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                                // 新しいビットマップにオリジナルのビットマップを2倍のサイズで描画します。
+                                graphics.DrawImage(image, new Rectangle(0, 0, zoomedBitmap.Width, zoomedBitmap.Height), new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+                                // PictureBoxに新しいビットマップを表示します。
+                                pictureBox1.Image = zoomedBitmap;
+                                pictureBox1.Invalidate();
+                            }));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            udpClient = new UdpClient(1024);
+            receiveThread = new Thread(new ThreadStart(ReceiveData));
+            receiveThread.Start();
+        }
+    }
+}
