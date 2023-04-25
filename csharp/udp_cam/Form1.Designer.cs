0985cf63a6899e2fbb8b2921038388438b041759
Atsushi Saitoh <lynxeyed@live.jp>
2023-04-25 09:41:50 +0900

    Update Form1.Designer.cs

--- csharp/udp_cam/Form1.Designer.cs
+++ csharp/udp_cam/Form1.Designer.cs
@@ -60,6 +60,7 @@
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
