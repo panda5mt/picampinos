aeea640b7e601bf23e07a86a2709e9b23c6240ae
panda5mt <lynxeyed@live.jp>
2023-06-10 01:30:55 +0900

    Automatically uploaded

--- firmware/arithmetic/pico_leq.c
+++ firmware/arithmetic/pico_leq.c
@@ -3,9 +3,9 @@
 
 // Gauss-Seidel
 #define MAX_ITERATION 5  // 最大反復回数
-void pico_gs(int32_t A[G_NUM][G_NUM], int32_t b[G_NUM], int32_t x[G_NUM]) {
+void pico_gs(float_t A[G_NUM][G_NUM], float_t b[G_NUM], float_t x[G_NUM]) {
     int i, j, k;
-    int32_t old_x[G_NUM];
+    float_t old_x[G_NUM];
     
     // 初期値の設定
     for (i = 0; i < G_NUM; i++) {
@@ -19,11 +19,11 @@ void pico_gs(int32_t A[G_NUM][G_NUM], int32_t b[G_NUM], int32_t x[G_NUM]) {
         }
         
         for (i = 0; i < G_NUM; i++) {
-            int32_t sum1 = 0.0;
+            float_t sum1 = 0.0;
             for (j = 0; j < i; j++) {
                 sum1 += A[i][j] * x[j];
             }
-            int32_t sum2 = 0.0;
+            float_t sum2 = 0.0;
             for (j = i + 1; j < G_NUM; j++) {
                 sum2 += A[i][j] * old_x[j];
             }
@@ -31,7 +31,7 @@ void pico_gs(int32_t A[G_NUM][G_NUM], int32_t b[G_NUM], int32_t x[G_NUM]) {
         }
 
         // 収束判定
-        int32_t error = 0.0;
+        float_t error = 0.0;
         for (i = 0; i < G_NUM; i++) {
             error += fabs(x[i] - old_x[i]);
         }
@@ -42,10 +42,10 @@ void pico_gs(int32_t A[G_NUM][G_NUM], int32_t b[G_NUM], int32_t x[G_NUM]) {
 }
 
 // Skyline 
-void forward_elimination(int32_t* A, int32_t* b, int* index, int n) {
+void forward_elimination(float_t* A, float_t* b, int* index, int n) {
     for (int k = 0; k < n-1; k++) {
         for (int i = k+1; i < n; i++) {
-            int32_t factor = A[index[i]+k] / A[index[k]+k];
+            float_t factor = A[index[i]+k] / A[index[k]+k];
             for (int j = k+1; j < n; j++) {
                 A[index[i]+j] -= factor * A[index[k]+j];
             }
@@ -54,7 +54,7 @@ void forward_elimination(int32_t* A, int32_t* b, int* index, int n) {
     }
 }
 
-void backward_substitution(int32_t* A, int32_t* b, int* index, int n, int32_t* x) {
+void backward_substitution(float_t* A, float_t* b, int* index, int n, float_t* x) {
     for (int i = n-1; i >= 0; i--) {
         x[i] = b[i];
         for (int j = i+1; j < n; j++) {
@@ -64,7 +64,7 @@ void backward_substitution(int32_t* A, int32_t* b, int* index, int n, int32_t* x
     }
 }
 
-void skyline_solve(int32_t* A, int32_t* b, int n, int32_t* x) {
+void skyline_solve(float_t* A, float_t* b, int n, float_t* x) {
     // 前処理: 非ゼロ要素の開始位置を示すインデクス配列を計算
     int* index = malloc(n * sizeof(int));
     int nz = 0;
