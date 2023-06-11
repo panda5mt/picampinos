53987b7417bda5c3e602c9c30fdeba70ebc07a58
panda5mt <lynxeyed@live.jp>
2023-06-11 16:45:31 +0900

    Automatically uploaded

--- firmware/main.c
+++ firmware/main.c
@@ -40,9 +40,9 @@
 #include "hardware/vreg.h"
 #include "hardware/i2c.h"
 #include "hardware/dma.h"
+#include "cam.h"
 //#include "hardware/regs/clocks.h"
 //#include "hardware/clocks.h"
-#include "cam.h"
 // #include "arithmetic/test_code.h"
 
 #define BOARD_LED           (28) // pico's led => 25, self made RP2040brd's led => 28. check hardware/RP2040Board.pdf 
