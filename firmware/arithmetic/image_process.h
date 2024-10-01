18acf3a93e698db7847e499f3dbb5d5142fdf619
panda5mt <lynxeyed@live.jp>
2024-10-01 13:44:30 -0400

    Automatically uploaded

--- firmware/arithmetic/image_process.h
+++ firmware/arithmetic/image_process.h
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include "pico/stdlib.h"
 
-void estimate_lightsource_with_sobel(int width, int height,
-                                     unsigned char *img_gray, float *p, float *q,
-                                     float *L, float *absL);
+void estimate_lightsource(int width, int height,
+                          unsigned char *img_gray, float *p, float *q,
+                          float *L, float *absL);
 
 void estimate_normal(int width, int height,
                      unsigned char *img_gray, float *p, float *q,
