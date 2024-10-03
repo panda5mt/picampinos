ef7109b657d93403401fde8970d055f517ddc56e
panda5mt <lynxeyed@live.jp>
2024-10-03 09:26:50 -0400

    Automatically uploaded

--- firmware/cam.h
+++ firmware/cam.h
@@ -23,15 +23,9 @@
 
 // camera buffer size
 // 640x480, RGB565 picture needs 640x480x2 bytes of buffers.
-// but RP2040 has no capacity such as huge buffers.
-// so, using DMA and IoT SRAM and lesser buffer to store them.
-// #define BLOCK (40)                                         // fixed: 40~80
-#define CAM_FUL_SIZE (640 * 480 * 2) // VGA size, RGB565(16bit) format
-// #define CAM_BUF_SIZE (640 * BLOCK * 2)                     // in bytes
-// #define CAM_BUF_HALF (CAM_BUF_SIZE / 2)                    // in bytes
-// #define NUM_COMP_FRM (CAM_FUL_SIZE / CAM_BUF_SIZE * 2) * 2 // (number of call by interrupt to complete Frame) * 2
-#define CAM_TOTAL_LEN (CAM_FUL_SIZE * 2)
-#define CAM_TOTAL_FRM (CAM_TOTAL_LEN / CAM_FUL_SIZE)
+#define CAM_FUL_SIZE (640 * 480 * 2)                 // VGA size, RGB565(16bit) format
+#define CAM_TOTAL_LEN (CAM_FUL_SIZE * 2)             // total length of pictures
+#define CAM_TOTAL_FRM (CAM_TOTAL_LEN / CAM_FUL_SIZE) // numbers(or frames) of pictures
 
 // high layer APIs
 void init_cam(uint8_t DEVICE_IS);
